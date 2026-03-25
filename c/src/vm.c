9d32efe7c75a2c3a6321ebf7e12e852234f74cd5
DavidGitoo <143304527+DavidGitoo@users.noreply.github.com>
2026-03-25 10:20:21 -0700

    forgot to save code files, FIXED lol

--- c/src/vm.c
+++ c/src/vm.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
@@ -110,8 +111,20 @@ static InterpretResult run() {
 }
 
 
-InterpretResult interpret(Chunk* chunk) {
-  vm.chunk = chunk;
+InterpretResult interpret(const char* source) {
+  Chunk chunk;
+  initChunk(&chunk);
+
+  if (!compile(source, &chunk)) {
+    freeChunk(&chunk);
+    return INTERPRET_COMPILE_ERROR;
+  }
+
+  vm.chunk = &chunk;
   vm.ip = vm.chunk->code;
-  return run();
+
+  InterpretResult result = run();
+
+  freeChunk(&chunk);
+  return result;
 }
